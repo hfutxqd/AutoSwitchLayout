173084db41e836fe0f9565d6524bbe58b6458223
xuqd <xuqd@getui.com>
2020-12-17 19:51:22 +0800

    More flexibility

--- autoswitchlayout/src/main/java/xyz/imxqd/autoswitchlayout/AutoSwitchLayout.java
+++ autoswitchlayout/src/main/java/xyz/imxqd/autoswitchlayout/AutoSwitchLayout.java
@@ -12,15 +12,21 @@ import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
 
+import java.lang.reflect.Field;
+
 
 public class AutoSwitchLayout extends LinearLayout implements AdapterView.OnItemSelectedListener, RadioGroup.OnCheckedChangeListener, OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
 
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     private static final String TAG = "AutoSwitchLayout";
 
-    AdapterView.OnItemSelectedListener listener = null;
+    AdapterView.OnItemSelectedListener mItemSelectedListener = null;
+    CompoundButton.OnCheckedChangeListener mCBCheckedChangeListener = null;
+    RadioGroup.OnCheckedChangeListener mRGCheckedChangeListener = null;
+    OnItemSelectedListener mOnItemSelectedListener2 = null;
 
     int targetId = 0;
+
 
     public AutoSwitchLayout(Context context) {
         super(context);
@@ -80,18 +86,39 @@ public class AutoSwitchLayout extends LinearLayout implements AdapterView.OnItem
     public void attachTo(View targetView) {
         if (targetView instanceof AdapterView) {
             hideChildren();
-            ((AdapterView) targetView).setOnItemSelectedListener(this);
+            try {
+                Field mOnItemSelectedListenerField = AdapterView.class.getDeclaredField("mOnItemSelectedListener");
+                mOnItemSelectedListenerField.setAccessible(true);
+                this.mItemSelectedListener = (AdapterView.OnItemSelectedListener) mOnItemSelectedListenerField.get(targetView);
+                ((RadioGroup) targetView).setOnCheckedChangeListener(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             onItemSelected((AdapterView<?>) targetView, ((AdapterView) targetView).getSelectedView(),
                     ((AdapterView) targetView).getSelectedItemPosition(), ((AdapterView) targetView).getSelectedItemId());
             Log.d(TAG, "attached to AdapterView");
         } else if (targetView instanceof RadioGroup) {
             hideChildren();
-            ((RadioGroup) targetView).setOnCheckedChangeListener(this);
+            try {
+                Field mOnCheckedChangeListenerField = RadioGroup.class.getDeclaredField("mOnCheckedChangeListener");
+                mOnCheckedChangeListenerField.setAccessible(true);
+                this.mRGCheckedChangeListener = (RadioGroup.OnCheckedChangeListener) mOnCheckedChangeListenerField.get(targetView);
+                ((RadioGroup) targetView).setOnCheckedChangeListener(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             onCheckedChanged((RadioGroup) targetView, ((RadioGroup) targetView).getCheckedRadioButtonId());
             Log.d(TAG, "attached to RadioGroup");
         } else if (targetView instanceof CompoundButton) {
             hideChildren();
-            ((CompoundButton) targetView).setOnCheckedChangeListener(this);
+            try {
+                Field mOnCheckedChangeListenerField = CompoundButton.class.getDeclaredField("mOnCheckedChangeListener");
+                mOnCheckedChangeListenerField.setAccessible(true);
+                this.mCBCheckedChangeListener = (CompoundButton.OnCheckedChangeListener) mOnCheckedChangeListenerField.get(targetView);
+                ((CompoundButton) targetView).setOnCheckedChangeListener(this);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             onCheckedChanged((CompoundButton) targetView, ((CompoundButton) targetView).isChecked());
 
             Log.d(TAG, "attached to CompoundButton");
@@ -101,13 +128,18 @@ public class AutoSwitchLayout extends LinearLayout implements AdapterView.OnItem
     }
 
     public void attachTo(SelectedItemObservable observable) {
+        this.mOnItemSelectedListener2 = observable.getOnItemSelectedListener();
         observable.setOnItemSelectedListener(this);
+        hideChildren();
         onItemSelected(observable.getSelectedId(), observable.getSelectedPosition());
     }
 
     @SuppressLint("CustomViewStyleable")
     public void onItemSelected(int id, int pos) {
         Log.d(TAG, String.format("onItemSelected:%d %d", id, pos));
+        if (this.mOnItemSelectedListener2 != null) {
+            this.mOnItemSelectedListener2.onItemSelected(id, pos);
+        }
         int count = getChildCount();
         for (int i = 0;i < count; i++) {
             View view = getChildAt(i);
@@ -123,6 +155,9 @@ public class AutoSwitchLayout extends LinearLayout implements AdapterView.OnItem
 
     @Override
     public void onNothingSelected() {
+        if (this.mOnItemSelectedListener2 != null) {
+            this.mOnItemSelectedListener2.onNothingSelected();
+        }
         onItemSelected(R.id.autoswitch_selected_none, -1);
     }
 
@@ -146,14 +181,26 @@ public class AutoSwitchLayout extends LinearLayout implements AdapterView.OnItem
         return new LayoutParams(p);
     }
 
+    public void setOnItemSelectedListener(OnItemSelectedListener listener) {
+        this.mOnItemSelectedListener2 = listener;
+    }
+
     public void setOnItemSelectedListener(AdapterView.OnItemSelectedListener listener) {
-        this.listener = listener;
+        this.mItemSelectedListener = listener;
+    }
+
+    public void setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener listener) {
+        this.mCBCheckedChangeListener = listener;
+    }
+
+    public void setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener listener) {
+        this.mRGCheckedChangeListener = listener;
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if (this.listener != null) {
-            this.listener.onItemSelected(parent, view, position, id);
+        if (this.mItemSelectedListener != null) {
+            this.mItemSelectedListener.onItemSelected(parent, view, position, id);
         }
         if (view != null) {
             onItemSelected(view.getId(), position);
@@ -164,20 +211,26 @@ public class AutoSwitchLayout extends LinearLayout implements AdapterView.OnItem
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        if (this.listener != null) {
-            this.listener.onNothingSelected(parent);
+        if (this.mItemSelectedListener != null) {
+            this.mItemSelectedListener.onNothingSelected(parent);
         }
         onNothingSelected();
     }
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (this.mRGCheckedChangeListener != null) {
+            this.mRGCheckedChangeListener.onCheckedChanged(group, checkedId);
+        }
         View v = group.findViewById(checkedId);
         onItemSelected(checkedId, group.indexOfChild(v));
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (this.mCBCheckedChangeListener != null) {
+            this.mCBCheckedChangeListener.onCheckedChanged(buttonView, isChecked);
+        }
         if (isChecked) {
             onItemSelected(R.id.autoswitch_checked, -2);
         } else {
@@ -189,6 +242,7 @@ public class AutoSwitchLayout extends LinearLayout implements AdapterView.OnItem
         int getSelectedId();
         int getSelectedPosition();
         void setOnItemSelectedListener(OnItemSelectedListener listener);
+        OnItemSelectedListener getOnItemSelectedListener();
     }
 
     public static class LayoutParams extends LinearLayout.LayoutParams {
